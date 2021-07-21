62d040388c6880d7cc72651826cc725300214aba
jianran.tfh <jianran.tfh@taobao.com>
2021-07-21 18:31:36 +0800

    jr test

--- staging/src/k8s.io/apiserver/pkg/endpoints/handlers/get.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/handlers/get.go
@@ -107,6 +107,7 @@ func GetResource(r rest.Getter, scope *RequestScope) http.HandlerFunc {
 			if trace != nil {
 				trace.Step("About to Get from storage")
 			}
+			panic(fmt.Sprintf("jr test %v", r))
 			return r.Get(ctx, name, &options)
 		})
 }
